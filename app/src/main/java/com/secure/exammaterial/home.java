419e0ce343420a00f462101b7bc58b2e4c391b02
SatyamSoni23 <2018kucp1121@iiitkota.ac.in>
2020-10-08 01:58:39 +0530

    card view added

--- app/src/main/java/com/secure/exammaterial/home.java
+++ app/src/main/java/com/secure/exammaterial/home.java
@@ -1,14 +1,42 @@
 package com.secure.exammaterial;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class home extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
 
+public class home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+    NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        init();
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(home.this, drawerLayout, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.white));
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(home.this);
+    }
+    void init(){
+        drawerLayout = findViewById(R.id.drawer_layout);
+        toolbar = findViewById(R.id.toolbar);
+        navigationView = findViewById(R.id.navigation_view);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItems) {
+        return false;
     }
 }
