41eb36d295f174d79b0340e67742afed55d8d66c
SatyamSoni23 <2018kucp1121@iiitkota.ac.in>
2020-10-09 22:33:48 +0530

    login

--- app/src/main/java/com/secure/exammaterial/MainActivity.java
+++ app/src/main/java/com/secure/exammaterial/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, home.class);
+        Intent intent = new Intent(this, login.class);
         startActivity(intent);
     }
 }
