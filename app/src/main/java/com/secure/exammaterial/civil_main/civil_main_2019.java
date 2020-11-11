e39060fc1071fca853cb8e0ab82ca9d048391e63
SatyamSoni23 <2018kucp1121@iiitkota.ac.in>
2020-11-12 01:16:42 +0530

    civil_main

---
+++ app/src/main/java/com/secure/exammaterial/civil_main/civil_main_2019.java
@@ -0,0 +1,16 @@
+package com.secure.exammaterial.civil_main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.secure.exammaterial.R;
+
+public class civil_main_2019 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_civil_main_2019);
+    }
+}
